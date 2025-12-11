49041db4e15134071c2dd33b8df80e5b3725d74b
Miklós Fazekas <mfazekas@szemafor.com>
2025-12-11 12:04:14 +0100

    Remove unnecessary empty init() methods and autolinking entries for ViewModel classes (#68)

--- ios/HybridViewModelBooleanProperty.swift
+++ ios/HybridViewModelBooleanProperty.swift
@@ -9,15 +9,7 @@ class HybridViewModelBooleanProperty: HybridViewModelBooleanPropertySpec, Valued
     self.property = property
     super.init()
   }
-  
-  /// ⚠️ DO NOT REMOVE
-  /// Nitro requires a parameterless initializer for JS bridging.
-  /// This is invoked automatically during hybrid module construction.
-  /// Internally we always use `init(property:)`
-  override init() {
-    super.init()
-  }
-  
+
   var value: Bool {
     get {
       return property.value
