17b6dc7d63f8092dca00fd86d362edd8fefabc0e
Miklós Fazekas <mfazekas@szemafor.com>
2026-01-12 10:25:23 +0100

    refactor: remove duplicate in-app test runner
    Keep only the harness tests in __tests__/rive.harness.ts

--- example/src/testing/index.ts
+++
@@ -1,1 +0,0 @@
-export type { TestCase, TestResult, TestStatus, TestSuite } from './types';
--- example/src/testing/suites/index.ts
+++
@@ -1,4 +0,0 @@
-import type { TestSuite } from '../types';
-import { viewModelTests } from './viewModelTests';
-
-export const allSuites: TestSuite[] = [viewModelTests];
