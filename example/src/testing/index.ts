0230868e08bacb7436ce50a43730436d7b24f0a5
Miklós Fazekas <mfazekas@szemafor.com>
2026-01-14 16:50:20 +0100

    refactor(testing): restore registry approach (harness doesn't export collectTests)

--- example/src/testing/index.ts
+++ example/src/testing/index.ts
@@ -2,5 +2,4 @@ export type { TestCase, TestSuite } from './registry';
 export { getSuites } from './registry';
 export type { TestStatus } from './types';
 
-// Import tests to register them
 import './tests';
