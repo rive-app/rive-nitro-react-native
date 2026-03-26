44bdc3b3b876aec45507a8a5bca12f5ed81119ef
Miklós Fazekas <mfazekas@szemafor.com>
2026-03-26 10:11:09 +0100

    fix: add missing createInstanceAsync to experimental backends

--- ios/new/HybridViewModel.swift
+++ ios/new/HybridViewModel.swift
@@ -87,4 +87,8 @@ class HybridViewModel: HybridViewModelSpec {
   func createBlankInstanceAsync() throws -> Promise<(any HybridViewModelInstanceSpec)?> {
     return Promise.async { try await self.createInstanceImpl() }
   }
+
+  func createInstanceAsync() throws -> Promise<(any HybridViewModelInstanceSpec)?> {
+    return try createBlankInstanceAsync()
+  }
 }
